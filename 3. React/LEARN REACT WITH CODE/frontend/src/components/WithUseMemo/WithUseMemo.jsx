893e8ce944c28171964de62c8ba4baaf1b9e0bbd
Manoj Katuwal <mkc684867@gmail.com>
2026-07-09 21:03:23 +0545

    use memo hook has been learned doing some heavey calculation using use memo hook

--- 3. React/LEARN REACT WITH CODE/frontend/src/components/WithUseMemo/WithUseMemo.jsx
+++ 3. React/LEARN REACT WITH CODE/frontend/src/components/WithUseMemo/WithUseMemo.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useMemo, useState } from "react";
 
 const WithUseMemo = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const expensiveCalculation = (num) => {
+    console.log("function learning");
+    let total = 0;
+
+    for (let i = 0; i < 100000000; i++) {
+      total = total + i;
+    }
+    return total + num;
+  };
+
+  const result = useMemo(() => {
+    return expensiveCalculation(count);
+  }, [count, text]);
   return (
     <div>
-      WITH OK 
+      <h2>Result : {result}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase Count</button>
+      <input
+        type="text "
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></input>
+      <p>Count : {count}</p>
     </div>
-  )
-}
+  );
+};
 
-export default WithUseMemo
+export default WithUseMemo;
