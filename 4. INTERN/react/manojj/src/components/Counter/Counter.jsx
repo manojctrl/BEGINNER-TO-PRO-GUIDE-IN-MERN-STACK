170453f95d9693ac81a930ca17dbbe5da8f8a26a
Manoj Katuwal <mkc684867@gmail.com>
2026-07-09 16:20:23 +0545

    auth context has been learned

--- 4. INTERN/react/manojj/src/components/Counter/Counter.jsx
+++ 4. INTERN/react/manojj/src/components/Counter/Counter.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { useCounter } from "../../context/CounterContext";
 
 const Counter = () => {
   const [count, setCount] = useState(1);
+  const {count, increment, decrement,reset} = useCounter()
   return (
     <main className="flex min-h-screen flex-col items-center  p-2 mt-20">
       <h1 className="text-center text-2xl font-bold">Counter</h1>
@@ -10,24 +12,20 @@ const Counter = () => {
 
       <div className="flex gap-4">
         <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
+          onClick={() => increment}
           className="px-4 py-2 bg-blue-500 text-white rounded"
         >
           Increment
         </button>
         <button
-          onClick={() => {
-            setCount(0);
-          }}
+          onClick={() => reset
+          }
           className="px-4 py-2 bg-gray-500 text-white rounded"
         >
           Reset
         </button>
         <button
-          onClick={() => {
-            setCount(count - 1);
+          onClick={() => { decrement
           }}
           className="px-4 py-2 bg-red-500 text-white rounded"
         >
