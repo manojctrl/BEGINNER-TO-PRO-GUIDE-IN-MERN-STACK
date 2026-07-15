f7bd7750f8c67a0eecb2fd924b9c740448971080
Manoj Katuwal <mkc684867@gmail.com>
2026-07-15 15:54:52 +0545

    leearning role based access using input form

--- 4. INTERN/routes/frontend/src/pages/Login.jsx
+++ 4. INTERN/routes/frontend/src/pages/Login.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div className='mt-10'>
-      <h1>Login Page</h1>
-    </div>
-  )
-}
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
 
-export default Login
+  const handleSubmit = (e) => {
+    // console.log(e);
+    e.preventDefault();
+    console.log(name);
+    console.log(role);
+  };
+
+  return (
+    <div className="mt-10">
+      <h1>Login Page</h1>
+      <form>
+        <div>
+          <label>Name </label>
+          <input
+            type="text"
+            placeholder="Enter your name "
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Role </label>
+          <input
+            type="text"
+            placeholder="Enter your role "
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="border border-amber-500 bg-amber-500 text-white px-4 py-2 rounded-md hover:bg-amber-600 transition"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
